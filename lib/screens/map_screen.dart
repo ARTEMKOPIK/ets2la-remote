062ff2becf73566f811a95932d6d9e682b8a9184
Artem <artemkopik@users.noreply.github.com>
2026-04-23 05:06:39 +0500

    fix(map): import telemetry models to resolve NavPosition/NavRoute build errors

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -3,6 +3,7 @@ import 'package:ets2la_remote/l10n/app_localizations.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
+import '../models/telemetry.dart';
 import '../providers/telemetry_provider.dart';
 import '../providers/settings_provider.dart';
 import '../theme/app_theme.dart';
